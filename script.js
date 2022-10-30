20aad23a2e5d93948553a0814335f25d632acc9e
die_narzisse <bulainarcis7@gmail.com>
2022-10-30 13:33:43 +0100

    special keys don't fire the countdown

--- script.js
+++ script.js
@@ -145,6 +145,7 @@ $(document).ready(function(event) {
     // events on keydown
     $("#text-input").keydown((e) => {
         keyboardAnimation(e);
+        if ((e.key).match(/[a-z]/i) && (e.key).length === 1)
         countdown(60);
     });
 
